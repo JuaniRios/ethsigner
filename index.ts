8331e837a3152f6b9625584df3bb03849f790dd5
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-10-23 15:16:02 +0200

    chore: remove unused deps

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-import { Wallet, getBytes, hashMessage, verifyMessage, toUtf8Bytes } from 'ethers';
-import { decodeAddress, cryptoWaitReady } from '@polkadot/util-crypto'; // Import the Polkadot.js utility
-import { u8aConcat, bnToU8a, stringToU8a, u8aToHex } from '@polkadot/util';
+import { Wallet, getBytes, hashMessage, verifyMessage } from 'ethers';
+import { cryptoWaitReady } from '@polkadot/util-crypto'; // Import the Polkadot.js utility
+import { u8aToHex } from '@polkadot/util';
 import { Keyring } from '@polkadot/keyring';
 
 const polimecAccount = '57qwwU823gziGcpWcQB8a3Pycp3a14xCUCPmd75jJXK5QArX';
 const projectId = 0;
 const nonce = 0;
-
-
 
 // Concatenate the byte arrays with the ASCII hyphen as a separator
 const message = `polimec account: ${polimecAccount} - project id: ${projectId} - nonce: ${nonce}`;
@@ -20,24 +18,19 @@ const account = wallet.address;
 const eipCompatibleMessage = hashMessage(message);
 const signature = await wallet.signMessage(message);
 
-console.log(
-	"Message:",
-	message,
-);
+console.log('Message:', message);
 console.log('EIP-191 Hashed Message:', eipCompatibleMessage);
 console.log('Ethereum Account:', account); // Print Ethereum account
 console.log('Signature:', signature); // Print the signature
 
-
-// Verify the Etherum signature
+// Verify the Ethereum signature
 if (getBytes(signature).length === 65) {
 	console.log('Signature is 65 bytes long');
 } else {
 	console.error('Signature length is incorrect:', getBytes(signature).length);
 }
 
-const isValidationSuccessful =
-	verifyMessage(message, signature) === account;
+const isValidationSuccessful = verifyMessage(message, signature) === account;
 console.log(
 	'Ether.js validation:',
 	isValidationSuccessful ? '✅ Success' : 'Failure',
@@ -53,9 +46,7 @@ const polkadot_signature = alice.sign(message);
 const isValid = alice.verify(message, polkadot_signature, alice.publicKey);
 
 if (getBytes(polkadot_signature).length === 64) {
-	console.log(
-		`✅ The Polkadot.js signature is ${isValid ? 'valid' : 'invalid'}`,
-	);
+	console.log('Polkadot.js validation:', isValid ? '✅ Success' : 'Failure');
 	console.log('Polkadot.js signature:', u8aToHex(polkadot_signature));
 	console.log(
 		`Signer's address: ${alice.address}, as hex: ${u8aToHex(alice.publicKey)}`,
